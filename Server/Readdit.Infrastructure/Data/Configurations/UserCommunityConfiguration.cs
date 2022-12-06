9f4e588206a15b86a46f5a340935962cc85dfc55
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-06 23:51:18 +0200

    Adding User Community and User Profile configurations ...

---
+++ Server/Readdit.Infrastructure/Data/Configurations/UserCommunityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Infrastructure.Data.Configurations;
+
+public class UserCommunityConfiguration : IEntityTypeConfiguration<UserCommunity>
+{
+    public void Configure(EntityTypeBuilder<UserCommunity> builder)
+    {
+        builder.HasKey(uc => new { uc.CommunityId, uc.UserId });
+    }
+}
---
+++ Server/Readdit.Infrastructure/Data/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Infrastructure.Data.Configurations;
+
+public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+{
+    public void Configure(EntityTypeBuilder<UserProfile> builder)
+    {
+    }
+}
