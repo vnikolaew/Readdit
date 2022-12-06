45f56720438e64d8118a2502a6fc6df94061240a
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-06 23:49:16 +0200

    Adding common helper components around DbContext configuration ...

---
+++ Server/Readdit.Infrastructure/Data/EntityIndicesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Models;
+
+namespace Readdit.Infrastructure.Data;
+
+public static class EntityIndicesConfiguration
+{
+    public static void Configure(ModelBuilder builder)
+    {
+        var deletableEntityTypes = builder
+            .Model
+            .GetEntityTypes()
+            .Where(et => typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+        
+        foreach (var deletableEntityType in deletableEntityTypes)
+        {
+            builder
+                .Entity(deletableEntityType.ClrType)
+                .HasIndex(nameof(IDeletableEntity.IsDeleted));
+        }
+    }
+    
+}
---
+++ Server/Readdit.Infrastructure/Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Readdit.Infrastructure.Data.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static IEnumerable<Type> GetEntityTypes<TEntity>(this ModelBuilder builder)
+        where TEntity : class
+        => builder
+            .Model
+            .GetEntityTypes()
+            .Where(et => typeof(TEntity).IsAssignableFrom(et.ClrType))
+            .Select(et => et.ClrType);
+}
