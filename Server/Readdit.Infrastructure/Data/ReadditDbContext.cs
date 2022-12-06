c520d16ab5bdbee6279066008a0cda69748b03ed
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-06 23:52:38 +0200

    Adding the application's Db Context holding all the entity models ...

---
+++ Server/Readdit.Infrastructure/Data/ReadditDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Models;
+using Readdit.Infrastructure.Data.Extensions;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Infrastructure.Data;
+
+public class ReadditDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+{
+    private static readonly MethodInfo SetIsDeletedQueryFilterMethod =
+        typeof(ReadditDbContext)
+            .GetMethod(nameof(SetIsDeletedQueryFilterMethod),
+                BindingFlags.NonPublic | BindingFlags.Static)!;
+    
+    public ReadditDbContext(DbContextOptions<ReadditDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<UserProfile> UserProfiles { get; set; }
+
+    public DbSet<Community> Communities { get; set; }
+
+    public DbSet<CommunityPost> CommunityPosts { get; set; }
+
+    public DbSet<CommunityTag> CommunityTags { get; set; }
+
+    public DbSet<PostComment> PostComments { get; set; }
+
+    public DbSet<PostTag> PostTags { get; set; }
+
+    public DbSet<PostVote> PostVotes { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<CommentVote> CommentVotes { get; set; }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        EntityIndicesConfiguration.Configure(builder);
+        
+        var entityTypes = builder.Model.GetEntityTypes().ToList();
+
+        var deletableTypes = builder.GetEntityTypes<IDeletableEntity>();
+        foreach (var deletableType in deletableTypes)
+        {
+            var method = SetIsDeletedQueryFilterMethod.MakeGenericMethod(deletableType);
+            method.Invoke(null, new object[] { builder });
+        }
+
+        var foreignKeys = entityTypes
+            .SelectMany(e => e.GetForeignKeys().Where(f => f.DeleteBehavior == DeleteBehavior.Cascade));
+        foreach (var foreignKey in foreignKeys)
+        {
+            foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+
+    private static void SetIsDeletedFilter<T>(ModelBuilder builder)
+        where T : class, IDeletableEntity
+        => builder.Entity<T>().HasQueryFilter(e => !e.IsDeleted);
+}
