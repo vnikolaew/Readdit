c357ca86efc2331480ebb1acaff638c6c436f032
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-06 23:55:13 +0200

    Adding an external Image / File Upload Service / Component needed for storing all application media content ...

---
+++ Server/Readdit.Services/External/Cloudinary/CloudinaryService.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Readdit.Services.External.Cloudinary;
+
+public class CloudinaryService : ICloudinaryService
+{
+    private readonly string[] _validTypes =
+    {
+        "image/x-png", "image/gif", "image/jpeg", "image/jpg", "image/png", "image/gif", "image/svg",
+    };
+
+    private readonly CloudinaryDotNet.Cloudinary _cloudinary;
+
+    public CloudinaryService(CloudinaryDotNet.Cloudinary cloudinary)
+    {
+        _cloudinary = cloudinary;
+    }
+
+    public bool IsFileValid(string contentType)
+    {
+        return _validTypes.Any(t => t == contentType);
+    }
+
+    public async Task<string> UploadAsync(Stream fileStream, string fileName, string contentType)
+    {
+        if (fileStream is null || !IsFileValid(contentType))
+        {
+            return string.Empty;
+        }
+
+        fileName += DateTime.UtcNow.ToString(CultureInfo.InvariantCulture);
+
+        await using var memoryStream = new MemoryStream();
+        await fileStream.CopyToAsync(memoryStream);
+        
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(fileName, memoryStream),
+            PublicId = fileName
+        };
+        
+        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+        return uploadResult.SecureUrl.AbsoluteUri;
+    }
+}
---
+++ Server/Readdit.Services/External/Cloudinary/ICloudinaryService.cs
@@ -0,0 +1,8 @@
+﻿namespace Readdit.Services.External.Cloudinary;
+
+public interface ICloudinaryService
+{
+    bool IsFileValid(string contentType);
+
+    Task<string> UploadAsync(Stream fileStream, string fileName, string contentType);
+}
