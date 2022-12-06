a9bccab6c7dbfa55a5cc36c9fbbf3555ed9fd2f5
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-06 23:56:37 +0200

    Adding Login and Register Input Models used by the Auth Service ...

---
+++ Server/Readdit.Services/Data/Authentication/Models/LoginInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readdit.Services.Data.Authentication.Models;
+
+public class LoginInputModel
+{
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
---
+++ Server/Readdit.Services/Data/Authentication/Models/RegisterInputModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Readdit.Services.Data.Authentication.Models;
+
+public class RegisterInputModel
+{
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [MaxLength(30)]
+    public string Password { get; set; }
+
+    [Required]
+    public string Country { get; set; }
+    
+    [Required]
+    public string Gender { get; set; }
+    
+    public IFormFile? ProfilePicture { get; set; }
+}
