848d51a58f975bb930276e6cafd2ceca6c460e4e
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:22:46 +0200

    Adding a new Common Project that is going to be used and shared across all other layers / projects ...

---
+++ Server/Readdit.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Readdit.Common;
+
+public static class GlobalConstants
+{
+    public const string AdministratorRoleName = "Admin";
+    public const string RegularUserRoleName = "RegularUser";
+    
+    public static class Seeding
+    {
+        public static readonly string
+            CountryDataPath = @"../Readdit.Infrastructure/Data/Seeding/Data/Countries.json";
+    }
+}
--- Server/Readdit.Infrastructure/Models/Country.cs
+++ Server/Readdit.Infrastructure/Models/Country.cs
@@ -5,7 +5,16 @@ namespace Readdit.Infrastructure.Models;
 
 public class Country : BaseEntity<string>
 {
+    public Country()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+    
     [Required]
     [MaxLength(50)]
     public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(10)]
+    public string Code { get; set; }
 }
