b0f4cd8d8c9de0f2fd913dd64ce39b8294f573f7
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:35:20 +0200

    Adding an Users Controller ...

---
+++ Server/Readdit.Web/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readdit.Services.Data.Users;
+using Readdit.Services.Data.Users.Models;
+using Readdit.Web.Infrastructure.Extensions;
+
+namespace Readdit.Web.Controllers;
+
+public class UsersController : ApiController
+{
+    private readonly IUsersService _usersService;
+
+    public UsersController(IUsersService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpGet]
+    [Route("{userId}")]
+    [ResponseCache(Duration = 60 * 10, Location = ResponseCacheLocation.Any)]
+    public async Task<IActionResult> Details([FromRoute]string userId)
+    {
+        var userDetails = await _usersService.GetUserInfoAsync<UserDetailsServiceModel>(userId);
+        return userDetails.OkOrNotFound();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateProfile(
+        [FromForm] UpdateUserProfileModel model) 
+    {
+        var user = await _usersService.UpdateUserProfileAsync(
+            User.GetId()!,
+            model.FirstName,
+            model.LastName,
+            model.Gender,
+            model.Country,
+            model.AboutContent,
+            model.Picture);
+        
+        return NoContent();
+    }
+}
