dbcaed214b8bab4bbde0e06f929e7b172eb3fdb5
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:30:09 +0200

    Database migrations - adding Country Code...

---
+++ Server/Readdit.Infrastructure/Data/Migrations/20221207184724_Adding Country Code ....cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Readdit.Infrastructure.Data.Migrations
+{
+    public partial class AddingCountryCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Communities_AspNetUsers_AdminId",
+                table: "Communities");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Countries",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Communities_AspNetUsers_AdminId",
+                table: "Communities",
+                column: "AdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Communities_AspNetUsers_AdminId",
+                table: "Communities");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Countries");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Communities_AspNetUsers_AdminId",
+                table: "Communities",
+                column: "AdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
