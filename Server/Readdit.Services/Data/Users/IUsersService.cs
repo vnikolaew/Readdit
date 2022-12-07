91d08e3936801e40a6c34fd8597c2ea3c94f6843
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 11:36:39 +0200

    Adding a Users Service for retrieving / updating User / Profile information ...

---
+++ Server/Readdit.Services/Data/Users/IUsersService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.Users;
+
+public interface IUsersService
+{
+    Task<ApplicationUser?> GetUserByIdAsync(string id);
+    
+    Task<T?> GetUserInfoAsync<T>(string id);
+
+    Task<ApplicationUser> UpdateUserProfileAsync(
+        string id,
+        string firstName,
+        string lastName,
+        string gender,
+        string country,
+        string aboutContent, 
+        IFormFile? profilePicture
+    );
+}
---
+++ Server/Readdit.Services/Data/Users/UsersService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+using Readdit.Infrastructure.Models.Enums;
+using Readdit.Services.External.Cloudinary;
+using Readdit.Services.Mapping;
+
+namespace Readdit.Services.Data.Users;
+
+public class UsersService : IUsersService
+{
+    private readonly IRepository<ApplicationUser> _users;
+    private readonly ICloudinaryService _cloudinaryService;
+    private readonly IRepository<Country> _countries;
+
+    public UsersService(
+        IRepository<ApplicationUser> users,
+        ICloudinaryService cloudinaryService,
+        IRepository<Country> countries)
+    {
+        _users = users;
+        _cloudinaryService = cloudinaryService;
+        _countries = countries;
+    }
+
+    public Task<ApplicationUser?> GetUserByIdAsync(string id)
+        => _users.All().FirstOrDefaultAsync(u => u.Id == id);
+
+    public Task<T?> GetUserInfoAsync<T>(string id)
+        => _users
+            .All()
+            .Where(u => u.Id == id)
+            .To<T>()
+            .FirstOrDefaultAsync();
+
+    public async Task<ApplicationUser> UpdateUserProfileAsync(
+        string id, string firstName,
+        string lastName, string gender, string country,
+        string aboutContent, IFormFile? profilePicture)
+    {
+        var user = await GetUserByIdWithProfileAsync(id);
+
+        if (user is null)
+        {
+            return null;
+        }
+
+        user.FirstName = firstName;
+        user.LastName = lastName;
+        user.Gender = Enum.Parse<Gender>(gender);
+        user.Profile.AboutContent = aboutContent;
+
+        var existingCountry = await _countries
+            .All()
+            .FirstOrDefaultAsync(c => c.Name == country);
+        if (existingCountry is not null)
+        {
+            user.Country = existingCountry;
+        }
+        
+        if (profilePicture != null)
+        {
+            string newPictureUrl = await _cloudinaryService.UploadAsync(
+                profilePicture.OpenReadStream(),
+                profilePicture.FileName,
+                profilePicture.ContentType);
+            user.Profile.ProfilePictureUrl = newPictureUrl;
+        }
+
+        await _users.SaveChangesAsync();
+        return user;
+    }
+
+    private Task<ApplicationUser?> GetUserByIdWithProfileAsync(string id)
+        => _users
+            .All()
+            .Include(u => u.Profile)
+            .FirstOrDefaultAsync(u => u.Id == id);
+}
