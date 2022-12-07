16cdd90d9bf6287eb5c772d4dd78c56c7551af0e
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:30:31 +0200

    Adding Comment Votes Service ...

---
+++ Server/Readdit.Services/Data/CommentVotes/CommentVotesService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+using Readdit.Infrastructure.Models.Enums;
+
+namespace Readdit.Services.Data.CommentVotes;
+
+public class CommentVotesService : ICommentVotesService
+{
+    private readonly IRepository<CommentVote> _commentVotes;
+
+    public CommentVotesService(IRepository<CommentVote> commentVotes)
+    {
+        _commentVotes = commentVotes;
+    }
+
+    public async Task<CommentVote> UpVoteAsync(string userId, string commentId)
+    {
+        var commentVote = await _commentVotes
+            .All()
+            .FirstOrDefaultAsync(cv =>cv.UserId == userId && cv.CommentId == commentId);
+
+        if (commentVote is null)
+        {
+            commentVote = new CommentVote
+            {
+                UserId = userId,
+                CommentId = commentId,
+                Type = VoteType.Up
+            };
+            
+            _commentVotes.Add(commentVote);
+            await _commentVotes.SaveChangesAsync();
+
+            return commentVote;
+        }
+
+        if (commentVote.Type == VoteType.Up)
+        {
+            return commentVote;
+        }
+        
+        commentVote.Type = VoteType.Up;
+        _commentVotes.Update(commentVote);
+        await _commentVotes.SaveChangesAsync();
+
+        return commentVote;
+    }
+
+    public async Task<CommentVote> DownVoteAsync(string userId, string commentId)
+    {
+        var commentVote = await _commentVotes
+            .All()
+            .FirstOrDefaultAsync(cv =>cv.UserId == userId && cv.CommentId == commentId);
+
+        if (commentVote is null)
+        {
+            commentVote = new CommentVote
+            {
+                UserId = userId,
+                CommentId = commentId,
+                Type = VoteType.Down
+            };
+            
+            _commentVotes.Add(commentVote);
+            await _commentVotes.SaveChangesAsync();
+
+            return commentVote;
+        }
+
+        if (commentVote.Type == VoteType.Down)
+        {
+            return commentVote;
+        }
+        
+        commentVote.Type = VoteType.Down;
+        _commentVotes.Update(commentVote);
+        
+        await _commentVotes.SaveChangesAsync();
+        return commentVote;
+    }
+}
---
+++ Server/Readdit.Services/Data/CommentVotes/ICommentVotesService.cs
@@ -0,0 +1,10 @@
+﻿using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.CommentVotes;
+
+public interface ICommentVotesService
+{
+    Task<CommentVote> UpVoteAsync(string userId, string commentId); 
+    
+    Task<CommentVote> DownVoteAsync(string userId, string commentId);
+}
