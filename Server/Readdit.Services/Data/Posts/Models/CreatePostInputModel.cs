201bbb4d7f85d121177ce9b40558938510f0b54d
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:36:43 +0200

    Adding Create / Update Post Service Models ...

---
+++ Server/Readdit.Services/Data/Posts/Models/CreatePostInputModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Readdit.Services.Data.Posts.Models;
+
+public class CreatePostInputModel
+{
+    [Required]
+    public string CommunityId { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; }
+    
+    [Required]
+    [MaxLength(1000)]
+    public string Content { get; set; }
+
+    public IEnumerable<string> Tags { get; set; } = new List<string>();
+    
+    public IFormFile? Media { get; set; }
+}
---
+++ Server/Readdit.Services/Data/Posts/Models/UpdatePostInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Readdit.Services.Data.Posts.Models;
+
+public class UpdatePostInputModel
+{
+    [Required]
+    public string PostId { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; }
+    
+    [Required]
+    [MaxLength(1000)]
+    public string Content { get; set; }
+    
+    public IFormFile Media { get; set; }
+}
