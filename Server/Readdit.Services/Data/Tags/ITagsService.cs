519cda7fd4a45442b38cf62e262bc0a4257f3bda
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:31:02 +0200

    Adding Tags Service ...

---
+++ Server/Readdit.Services/Data/Tags/ITagsService.cs
@@ -0,0 +1,10 @@
+﻿using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.Tags;
+
+public interface ITagsService
+{
+    Task<IEnumerable<Tag>> GetAllAsync();
+    
+    Task<IEnumerable<Tag>> GetAllByNamesAsync(IEnumerable<string> tagNames);
+}
---
+++ Server/Readdit.Services/Data/Tags/TagsService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.Tags;
+
+public class TagsService : ITagsService
+{
+    private readonly IRepository<Tag> _tags;
+
+    public TagsService(IRepository<Tag> tags)
+    {
+        _tags = tags;
+    }
+
+    public async Task<IEnumerable<Tag>> GetAllAsync()
+        => await _tags.All().ToListAsync();
+
+    public async Task<IEnumerable<Tag>> GetAllByNamesAsync(IEnumerable<string> tagNames)
+        => await _tags.All().Where(t => tagNames.AsEnumerable().Contains(t.Name)).ToListAsync();
+}
