88807ac4d59c47cfc57f2594c02b229a9d10df92
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-07 23:35:40 +0200

    Adding an Communities Controller ...

---
+++ Server/Readdit.Web/Controllers/CommunitiesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readdit.Infrastructure.Models;
+using Readdit.Services.Data.Communities;
+using Readdit.Services.Data.Communities.Models;
+using Readdit.Web.Infrastructure.Extensions;
+
+namespace Readdit.Web.Controllers;
+
+public class CommunitiesController : ApiController
+{
+    private readonly ICommunityService _communityService;
+
+    public CommunitiesController(ICommunityService communityService)
+    {
+        _communityService = communityService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromForm] CreateCommunityInputModel model)
+    {
+        var community = await _communityService.CreateAsync(
+            User.GetId()!,
+            model.Name,
+            model.Description,
+            model.Picture,
+            model.Tags,
+            model.Type);
+
+        return community is null
+            ? BadRequest()
+            : CreatedAtAction(nameof(Details), new { communityId = community.Id }, community);
+    }
+    
+    [HttpGet]
+    [Route("{communityId}")]
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+    public async Task<IActionResult> Details([FromRoute]string communityId)
+    {
+        var post = await _communityService.GetCommunityDetailsByIdAsync<CommunityPost>(communityId);
+        return post.OkOrNotFound();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> Update([FromForm] UpdateCommunityInputModel model)
+    {
+        var community = await _communityService.UpdateAsync(
+            User.GetId()!,
+            model.CommunityId,
+            model.Description,
+            model.Picture,
+            model.Tags,
+            model.Type);
+
+        return community is null
+            ? BadRequest()
+            : Ok(community);
+    }
+
+    [HttpDelete]
+    [Route("{communityId}")]
+    public async Task<IActionResult> Delete([FromRoute] string communityId)
+    {
+        var success = await _communityService.DeleteAsync(User.GetId()!, communityId);
+        return success ? Ok() : BadRequest();
+    }
+}
