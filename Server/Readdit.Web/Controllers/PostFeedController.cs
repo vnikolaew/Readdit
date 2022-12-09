d43662b9becd72e120a888e283ed0b99c29b8596
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-09 17:29:57 +0200

    Adding a Post Feed Controller ...

---
+++ Server/Readdit.Web/Controllers/PostFeedController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Readdit.Infrastructure.Models;
+using Readdit.Services.Data.PostFeed;
+using Readdit.Services.Data.PostFeed.Enums;
+using Readdit.Web.Infrastructure.Extensions;
+
+namespace Readdit.Web.Controllers;
+
+public class PostFeedController : ApiController
+{
+    private readonly IPostFeedService _postFeedService;
+
+    public PostFeedController(IPostFeedService postFeedService)
+        => _postFeedService = postFeedService;
+
+    [HttpGet]
+    [Route("recent")]
+    public async Task<IActionResult> MostRecentPostsAsync()
+    {
+        var posts = await _postFeedService
+            .GetMostRecentForUserAsync<CommunityPost>(User.GetId()!);
+        return Ok(posts);
+    }
+    
+    [HttpGet]
+    [Route("top")]
+    public async Task<IActionResult> BesVotedPostsAsync([FromQuery] TimeRange range)
+    {
+        var posts = await _postFeedService
+            .GetBestVotedForUserAsync<CommunityPost>(User.GetId()!, range);
+        return Ok(posts);
+    }
+}
