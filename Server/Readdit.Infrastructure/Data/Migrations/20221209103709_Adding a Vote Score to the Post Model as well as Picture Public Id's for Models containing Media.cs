dd41de1226e10cb72601651d99025f71dc2f4fc8
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-09 17:32:01 +0200

    Adding previously unversioned Database migrations ...

---
+++ Server/Readdit.Infrastructure/Data/Migrations/20221209103709_Adding a Vote Score to the Post Model as well as Picture Public Id's for Models containing Media.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Readdit.Infrastructure.Data.Migrations
+{
+    public partial class AddingaVoteScoretothePostModelaswellasPicturePublicIdsforModelscontainingMedia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicturePublicId",
+                table: "UserProfiles",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MediaPublicId",
+                table: "CommunityPosts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VoteScore",
+                table: "CommunityPosts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PicturePublicId",
+                table: "Communities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicturePublicId",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "MediaPublicId",
+                table: "CommunityPosts");
+
+            migrationBuilder.DropColumn(
+                name: "VoteScore",
+                table: "CommunityPosts");
+
+            migrationBuilder.DropColumn(
+                name: "PicturePublicId",
+                table: "Communities");
+        }
+    }
+}
---
+++ Server/Readdit.Infrastructure/Data/Migrations/20221209123557_Adding a Vote Score for Post Comments.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Readdit.Infrastructure.Data.Migrations
+{
+    public partial class AddingaVoteScoreforPostComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VoteScore",
+                table: "PostComments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VoteScore",
+                table: "PostComments");
+        }
+    }
+}
