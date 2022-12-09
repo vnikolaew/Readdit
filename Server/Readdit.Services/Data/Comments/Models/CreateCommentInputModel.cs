e3f1d850a8d5fa6824dd88d2c962782f6d1434d0
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-09 17:23:06 +0200

    Adding Comment Service Models ...

---
+++ Server/Readdit.Services/Data/Comments/Models/CreateCommentInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readdit.Services.Data.Comments.Models;
+
+public class CreateCommentInputModel
+{
+    [Required]
+    public string PostId { get; set; }
+    
+    [Required]
+    [MaxLength(2000)]
+    public string Content { get; set; }
+}
---
+++ Server/Readdit.Services/Data/Comments/Models/UpdateCommentInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Readdit.Services.Data.Comments.Models;
+
+public class UpdateCommentInputModel
+{
+    [Required]
+    public string CommentId { get; set; }
+    
+    [Required]
+    [MaxLength(2000)]
+    public string Content { get; set; }
+}
