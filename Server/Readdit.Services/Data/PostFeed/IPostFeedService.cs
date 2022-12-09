f49b6331b7ce35419ff4c60538b21f693bc235f6
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-09 17:27:36 +0200

    Adding a User Post Feed Service for generating the User's Front Page ...

---
+++ Server/Readdit.Services/Data/PostFeed/IPostFeedService.cs
@@ -0,0 +1,12 @@
+﻿using Readdit.Services.Data.PostFeed.Enums;
+
+namespace Readdit.Services.Data.PostFeed;
+
+public interface IPostFeedService
+{
+    Task<IEnumerable<T>> GetMostRecentForUserAsync<T>(string userId);
+
+    Task<IEnumerable<T>> GetBestVotedForUserAsync<T>(
+        string userId,
+        TimeRange range);
+}
---
+++ Server/Readdit.Services/Data/PostFeed/PostFeedService.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+using Readdit.Infrastructure.Models.Enums;
+using Readdit.Services.Data.PostFeed.Enums;
+using Readdit.Services.Mapping;
+
+namespace Readdit.Services.Data.PostFeed;
+
+public class PostFeedService : IPostFeedService
+{
+    private readonly IRepository<CommunityPost> _posts;
+    private readonly IRepository<UserCommunity> _userCommunities;
+
+    public PostFeedService(
+        IRepository<CommunityPost> posts,
+        IRepository<UserCommunity> userCommunities)
+    {
+        _posts = posts;
+        _userCommunities = userCommunities;
+    }
+
+    public async Task<IEnumerable<T>> GetMostRecentForUserAsync<T>(string userId)
+    {
+        var communityIds = (await _userCommunities
+                .AllAsNoTracking()
+                .Where(uc => uc.UserId == userId
+                             && uc.Community.Type != CommunityType.Private)
+                .Select(uc => new { uc.CommunityId })
+                .ToListAsync())
+            .Select(c => c.CommunityId);
+
+        var posts = await _posts
+            .AllAsNoTracking()
+            .Where(p => communityIds.Contains(p.CommunityId))
+            .OrderByDescending(p => p.CreatedOn)
+            .ThenByDescending(p => p.Votes.Count)
+            .To<T>()
+            .ToListAsync();
+        
+        return posts;
+    }
+    
+    public async Task<IEnumerable<T>> GetBestVotedForUserAsync<T>(
+        string userId,
+        TimeRange range)
+    {
+        var communityIds = (await _userCommunities
+                .AllAsNoTracking()
+                .Where(uc => uc.UserId == userId
+                             && uc.Community.Type != CommunityType.Private)
+                .Select(uc => new { uc.CommunityId })
+                .ToListAsync())
+            .Select(c => c.CommunityId);
+
+        var startDate = range.GetStartDate();
+        var posts = await _posts
+            .AllAsNoTracking()
+            .Where(p => communityIds.Contains(p.CommunityId)
+                        && p.CreatedOn > startDate)
+            .OrderByDescending(p => p.Votes.Count)
+            .ThenByDescending(p => p.CreatedOn)
+            .To<T>()
+            .ToListAsync();
+        
+        return posts;
+    }
+}
