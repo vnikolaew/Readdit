e0892a3dda15815daf35dd63ae20b1de8799892c
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-11 00:27:10 +0200

    Adding a Post Comment Service Model ...

---
+++ Server/Readdit.Services/Data/Posts/Models/PostCommentAuthorModel.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Readdit.Infrastructure.Models;
+using Readdit.Services.Mapping;
+
+namespace Readdit.Services.Data.Posts.Models;
+
+public class PostCommentAuthorModel : IHaveCustomMappings
+{
+    public string Id { get; set; }
+    
+    public string UserName { get; set; }
+    
+    public string ProfilePictureUrl { get; set; }
+    
+    public string ProfilePicturePublicId { get; set; }
+    
+    public void CreateMappings(IProfileExpression configuration)
+        => configuration
+            .CreateMap<ApplicationUser, PostCommentAuthorModel>()
+            .ForMember(a => a.ProfilePictureUrl,
+                cfg => cfg.MapFrom(u => u.Profile.ProfilePictureUrl))
+            .ForMember(a => a.ProfilePicturePublicId,
+                cfg => cfg.MapFrom(u => u.Profile.ProfilePicturePublicId));
+}
---
+++ Server/Readdit.Services/Data/Posts/Models/PostCommentDetailsModel.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Readdit.Infrastructure.Models;
+using Readdit.Services.Data.PostFeed.Models;
+using Readdit.Services.Mapping;
+
+namespace Readdit.Services.Data.Posts.Models;
+
+public class PostCommentDetailsModel : IHaveCustomMappings
+{
+    public PostCommentAuthorModel Author { get; set; }
+    
+    public string Content { get; set; }
+    
+    public DateTime CreatedOn { get; set; }
+    
+    public DateTime? ModifiedOn { get; set; }
+    
+    public int VoteScore { get; set; }
+    
+    public UserVoteModel? UserVote { get; set; }
+    
+    public void CreateMappings(IProfileExpression configuration)
+    {
+        string? currentUserId = null;
+        configuration
+            .CreateMap<PostComment, PostCommentDetailsModel>()
+            .ForMember(pm => pm.UserVote,
+                cfg =>
+                    cfg.MapFrom(c => c.Votes.Where(v => v.UserId == currentUserId)
+                        .Select(v => new UserVoteModel { Id = v.Id, Type = v.Type })
+                        .FirstOrDefault()));
+    }
+}
