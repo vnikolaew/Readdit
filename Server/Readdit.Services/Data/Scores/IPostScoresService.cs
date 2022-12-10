50a27debf0782e7a6b5da4383429f205b850d880
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-11 00:25:31 +0200

    Adding a Posts Score Service ...

---
+++ Server/Readdit.Services/Data/Scores/IPostScoresService.cs
@@ -0,0 +1,8 @@
+﻿namespace Readdit.Services.Data.Scores;
+
+public interface IPostScoresService
+{
+    Task<bool> IncreaseForUserAsync(string userId, int amount);
+    
+    Task<bool> DecreaseForUserAsync(string userId, int amount);
+}
---
+++ Server/Readdit.Services/Data/Scores/PostScoresService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.Scores;
+
+public class PostScoresService : IPostScoresService
+{
+    private readonly IRepository<UserScore> _userScores;
+
+    public PostScoresService(IRepository<UserScore> userScores)
+        => _userScores = userScores;
+
+    public async Task<bool> IncreaseForUserAsync(string userId, int amount)
+    {
+        var userScore = await _userScores
+            .All()
+            .FirstOrDefaultAsync(us => us.Id == userId);
+        if (userScore is null)
+        {
+            return false;
+        }
+
+        userScore.PostsScore += amount;
+        return true;
+    }
+
+    public async Task<bool> DecreaseForUserAsync(string userId, int amount)
+    {
+        var userScore = await _userScores
+            .All()
+            .FirstOrDefaultAsync(us => us.Id == userId);
+        if (userScore is null)
+        {
+            return false;
+        }
+
+        userScore.PostsScore -= amount;
+        return true;
+    }
+}
