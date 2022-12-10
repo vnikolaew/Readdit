e64a36af03884b46248eebe9fbf650e66c0756e4
Victorio Nikolaev <vikinik01@abv.bg>
2022-12-11 00:24:56 +0200

    Adding a Comments Score Service ...

---
+++ Server/Readdit.Services/Data/Scores/CommentScoreService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readdit.Infrastructure.Common.Repositories;
+using Readdit.Infrastructure.Models;
+
+namespace Readdit.Services.Data.Scores;
+
+public class CommentScoreService : ICommentScoreService
+{
+    private readonly IRepository<UserScore> _userScores;
+
+    public CommentScoreService(IRepository<UserScore> userScores)
+        => _userScores = userScores;
+
+    public async Task<bool> IncreaseForUserAsync(string userId, int amount)
+    {
+        var userScore = await _userScores
+            .All()
+            .FirstOrDefaultAsync(us => us.Id == userId);
+        if (userScore is null)
+        {
+            return false;
+        }
+
+        userScore.CommentsScore += amount;
+        return true;
+    }
+
+    public async Task<bool> DecreaseForUserAsync(string userId, int amount)
+    {
+        var userScore = await _userScores
+            .All()
+            .FirstOrDefaultAsync(us => us.Id == userId);
+        if (userScore is null)
+        {
+            return false;
+        }
+
+        userScore.CommentsScore -= amount;
+        return true;
+    }
+}
---
+++ Server/Readdit.Services/Data/Scores/ICommentScoreService.cs
@@ -0,0 +1,8 @@
+﻿namespace Readdit.Services.Data.Scores;
+
+public interface ICommentScoreService
+{
+    Task<bool> IncreaseForUserAsync(string userId, int amount);
+    
+    Task<bool> DecreaseForUserAsync(string userId, int amount);
+}
